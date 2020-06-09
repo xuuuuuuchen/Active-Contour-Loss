f76737b92a2bea558f5a960bb1ef00bbe09b8457
Xu <41378501+xuuuuuuchen@users.noreply.github.com>
2020-06-09 18:22:48 +0100

    Update Active-Contour-Loss.py

--- Active-Contour-Loss.py
+++ Active-Contour-Loss.py
@@ -5,8 +5,6 @@ import numpy as np
 
 def Active_Contour_Loss(y_true, y_pred): 
 
-	#y_pred = K.cast(y_pred, dtype = 'float64')
-
 	"""
 	lenth term
 	"""
@@ -18,9 +16,7 @@ def Active_Contour_Loss(y_true, y_pred):
 	delta_y = y[:,:,:-2,1:]**2
 	delta_u = K.abs(delta_x + delta_y) 
 
-	epsilon = 0.00000001 # where is a parameter to avoid square root is zero in practice.
-	w = 1
-	lenth = w * K.sum(K.sqrt(delta_u + epsilon)) # equ.(11) in the paper
+	lenth = K.mean(K.sqrt(delta_u + 0.00000001)) # equ.(11) in the paper
 
 	"""
 	region term
@@ -29,11 +25,10 @@ def Active_Contour_Loss(y_true, y_pred):
 	C_1 = np.ones((256, 256))
 	C_2 = np.zeros((256, 256))
 
-	region_in = K.abs(K.sum( y_pred[:,0,:,:] * ((y_true[:,0,:,:] - C_1)**2) ) ) # equ.(12) in the paper
-	region_out = K.abs(K.sum( (1-y_pred[:,0,:,:]) * ((y_true[:,0,:,:] - C_2)**2) )) # equ.(12) in the paper
+	region_in = K.abs(K.mean( y_pred[:,0,:,:] * ((y_true[:,0,:,:] - C_1)**2) ) ) # equ.(12) in the paper
+	region_out = K.abs(K.mean( (1-y_pred[:,0,:,:]) * ((y_true[:,0,:,:] - C_2)**2) )) # equ.(12) in the paper
 
 	lambdaP = 1 # lambda parameter could be various.
+	mu = 1 # mu parameter could be various.
 	
-	loss =  lenth + lambdaP * (region_in + region_out) 
-
-	return loss
+	return lenth + lambdaP * (mu * region_in + region_out) 
